8d5ae1dcdb73ca35739d7857cc6bc63516a03ab7
Witold Sławko <witold.slawko@gmail.com>
2021-05-31 18:23:38 +0200

    Update get-params.sh

--- 08 - Analytics, Security, Costs/zad 01 - SSM/01-aws-cli/get-params.sh
+++ 08 - Analytics, Security, Costs/zad 01 - SSM/01-aws-cli/get-params.sh
@@ -1,8 +1,10 @@
-#~/bin/bash
+#!/bin/bash
 
 aws ssm get-parameter \
-    --name string-param-name-from-aws-cli 
+    --name string-param-name-from-aws-cli \
+    --profile panda
 
 aws ssm get-parameter \
     --name secure-string-param-name-from-aws-cli \
-   --with-decryption
+    --with-decryption \
+    --profile panda
