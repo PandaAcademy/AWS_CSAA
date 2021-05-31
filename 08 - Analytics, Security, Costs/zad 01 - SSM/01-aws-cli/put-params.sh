d3bdc4ff7acc3495fb838244dec9def22911abe1
Witold Sławko <witold.slawko@gmail.com>
2021-05-31 18:24:05 +0200

    Update put-params.sh

--- 08 - Analytics, Security, Costs/zad 01 - SSM/01-aws-cli/put-params.sh
+++ 08 - Analytics, Security, Costs/zad 01 - SSM/01-aws-cli/put-params.sh
@@ -1,11 +1,13 @@
-#~/bin/bash
+#!/bin/bash
 
 aws ssm put-parameter \
     --name string-param-name-from-aws-cli \
     --type String \
-    --value string-param-value-from-aws-cli
+    --value string-param-value-from-aws-cli \
+    --profile panda
 
 aws ssm put-parameter \
     --name secure-string-param-name-from-aws-cli \
     --type SescureString \
-    --value secure-string-value-from-aws-cli
+    --value secure-string-value-from-aws-cli \
+    --profile panda
